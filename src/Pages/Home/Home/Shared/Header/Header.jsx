371738f4c4f9f89cf429e67bef6e3a5d4dd7719f
ipptykhan <ipptykhancse@gmail.com>
2023-06-10 15:08:48 +0600

    right nav

--- src/Pages/Home/Home/Shared/Header/Header.jsx
+++ src/Pages/Home/Home/Shared/Header/Header.jsx
@@ -2,22 +2,43 @@ import React from 'react';
 import logo from '../../../../../assets/logo.png';
 import moment from 'moment';
 import Marquee from 'react-fast-marquee';
-import { Button } from 'react-bootstrap';
+import { Button, Container, Form, Nav, Navbar } from 'react-bootstrap';
 const Header = () => {
     return (
-        <div>
+        <Container>
            <div className='text-center'>
             <img src={logo} />
             <p className='text-secondary'> <small> Journalism without fear or favour</small></p>
             <p>{moment().format("dddd, MMMM D, YYYY")}</p>
            </div>
-           <div>
+           <div className='d-flex'>
            <Button variant="danger"> Latest </Button>
-            <Marquee>
-            I can be a React component, multiple React components, or just some text.
+            <Marquee className='text-danger' speed={100}>
+            I can be a React component, multiple React components, or just some text... I can be a React component, multiple React components, or just some text.
             </Marquee>
            </div>
-        </div>
+
+           <Navbar bg="light" expand="lg">
+      <Container fluid>
+        
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="mx-auto ">
+            <Nav.Link href="#action1">Home</Nav.Link>
+            <Nav.Link href="#action2">About</Nav.Link>
+            <Nav.Link href="#action2">Career</Nav.Link>
+            
+          </Nav>
+          <Form className="d-flex">
+          <Nav.Link className='ml-5' href="#action2">Profile </Nav.Link>
+            <Button variant="secondary">Login</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+
+        </Container>
     );
 };
 
--- src/Pages/Home/Home/Shared/RightNav/RightNav.jsx
+++ src/Pages/Home/Home/Shared/RightNav/RightNav.jsx
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import {FaGooglePlusG, FaGithub} from 'react-icons/fa';
 
 const RightNav = () => {
     return (
         <div>
-            <h3>Right Nav</h3>
+            <h4>Login With:</h4>
+            <Button className='mb-2' variant="outline-primary"> <FaGooglePlusG /> Login with Google</Button>
+            <br></br>
+            <Button variant="outline-secondary"> <FaGithub /> Login with Github</Button>
         </div>
     );
 };
