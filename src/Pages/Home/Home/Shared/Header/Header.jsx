dab27efb313554752f2b9d64903b52c83f810943
ipptykhan <ipptykhancse@gmail.com>
2023-06-10 14:30:02 +0600

    header update

--- src/Pages/Home/Home/Shared/Header/Header.jsx
+++ src/Pages/Home/Home/Shared/Header/Header.jsx
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import logo from '../../../../../assets/logo.png';
+import moment from 'moment';
+import Marquee from 'react-fast-marquee';
+import { Button } from 'react-bootstrap';
 const Header = () => {
     return (
         <div>
-           <h2>This is header....</h2> 
+           <div className='text-center'>
+            <img src={logo} />
+            <p className='text-secondary'> <small> Journalism without fear or favour</small></p>
+            <p>{moment().format("dddd, MMMM D, YYYY")}</p>
+           </div>
+           <div>
+           <Button variant="danger"> Latest </Button>
+            <Marquee>
+            I can be a React component, multiple React components, or just some text.
+            </Marquee>
+           </div>
         </div>
     );
 };
